955af881c22c0cafef306b3c3c7025b3bdf7b6d8
tito2909 <omaar415@gmail.com>
2023-07-16 02:36:17 +0300

    last touches

--- Class1.cs
+++ Class1.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading;
 using System.Collections.Generic;
 using System.Security.Cryptography.X509Certificates;
+using System.Windows;
+
 
 namespace server
 {
@@ -17,29 +19,47 @@ namespace server
         private static TcpClient thirdSideClient;
         private static NetworkStream thirdSideStream;
         private static byte[] thirdSideBuffer = new byte[4096];
+        public event EventHandler<EventData> SendData;
+
         public static void Main(string[] args)
+        {
+            Server server = new Server();
+            server.SendData += SendToThirdSide;
+            server.Start();
+           
+        }
+        public void Start()
         {
             tcpListener = new TcpListener(ipAddress, port);
             tcpListener.Start();
             Console.WriteLine("Server started.");
+            // Start a separate thread to receive data from the third side
+            Thread receiveFromThirdSideThread = new Thread(new ThreadStart(ReceiveFromThirdSide));
+            receiveFromThirdSideThread.Start();
 
             while (true)
             {
                 TcpClient client = tcpListener.AcceptTcpClient();
-                clientBuffers.Add(client, new byte[4096]);
+                if (client != null)
+                {
+                    clientBuffers.Add(client, new byte[4096]);
+                    Server server = new Server();
+                 
+                    Thread clientThread = new Thread(new ParameterizedThreadStart(HandleClient));
+                    clientThread.Start();
+                }
+                else
+                {
+                    Console.WriteLine("no client connected");
+                }
 
-                Thread clientThread = new Thread(new ParameterizedThreadStart(HandleClient));
-                clientThread.Start(client);
+                
 
-                Thread sendToThirdSideThread = new Thread(new ThreadStart(SendToThirdSide));
-                sendToThirdSideThread.Start();
-
-                // Start a separate thread to receive data from the third side
-                Thread receiveFromThirdSideThread = new Thread(new ThreadStart(ReceiveFromThirdSide));
-                receiveFromThirdSideThread.Start();
+               
+               
             }
         }
-
+          // method to receive from third side and send the data to the clients
         private static void ReceiveFromThirdSide()
         {
             while (true)
@@ -47,10 +67,12 @@ namespace server
                 try
                 {
                     byte[] buffer = new byte[4096];
-                    int bytesRead = thirdSideStream.Read(buffer, 0, buffer.Length);
-                    string receivedData = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+                    int bytesRead = thirdSideStream.Read(thirdSideBuffer, 0, buffer.Length);
+                    string receivedData = Encoding.ASCII.GetString(thirdSideBuffer, 0, bytesRead);
                     Console.WriteLine("Received from third side: " + receivedData);
+
                     // Send the received data to all connected clients
+                   
                     foreach (TcpClient connectedClient in clientBuffers.Keys)
                     {
                         NetworkStream connectedClientStream = connectedClient.GetStream();
@@ -67,83 +89,99 @@ namespace server
                 }
             }
         }
-                // method to send the clients  data stored in the buffer to the third side 
-                static void SendToThirdSide()
+        // method to send the clients  data stored in the buffer to the third side 
+        static void SendToThirdSide(object source, EventData args)
+        {
+            string thirdSideIPAddress = "ThirdSideIPAddress";
+            int thirdSidePort = 2808;
+            thirdSideClient = new TcpClient();
+
+            try
+            {
+                thirdSideClient.Connect(thirdSideIPAddress, thirdSidePort);
+                thirdSideStream = thirdSideClient.GetStream();
+
+                foreach (byte[] buffer in clientBuffers.Values)
                 {
-                    TcpClient thirdSideClient = new TcpClient();
-
-                    try
-                    {
-                        string thirdSideIPAddress = "ThirdSideIPAddress";
-                        int thirdSidePort = 2808;
-                        thirdSideClient.Connect("ThirdSideIPAddress", thirdSidePort);
-
-                        foreach (byte[] buffer in clientBuffers.Values)
-                        {
-                            // Send the buffer to the third side
-                            thirdSideClient.GetStream().Write(buffer, 0, buffer.Length);
-                        }
-
-                        Console.WriteLine("Buffers sent to the third side.");
-                    }
-                    catch (Exception ex)
-                    {
-                        Console.WriteLine("Error sending buffers to the third side: " + ex.Message);
-                    }
-                    finally
-                    {
-                        thirdSideClient.Close();
-                    }
+                    // Send the buffer to the third side
+                    thirdSideStream.Write(buffer, 0, buffer.Length);
                 }
 
+                Console.WriteLine("Buffers sent to the third side.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error sending buffers to the third side: " + ex.Message);
+            }
+            finally
+            {
+                thirdSideClient.Close();
+
+            }
+        }
 
 
-                // method to handle the client data , send and receive.
-                static void HandleClient(object obj)
-                {
-                    TcpClient client = (TcpClient)obj;
-                    NetworkStream clientStream = client.GetStream();
+        // method to handle the client data , send and receive.
+        void HandleClient(object obj)
+        {
+            TcpClient client = (TcpClient)obj;
+
+            NetworkStream clientStream = client.GetStream();
 
             byte[] buffer = clientBuffers[client]; // Get the client's buffer
             int bytesRead;
 
-                    while (true)
-                    {
-                        bytesRead = 0;
+            while (true)
+            {
+                bytesRead = 0;
 
-                        try
-                        {
-                            bytesRead = clientStream.Read(buffer, 0, buffer.Length);
-                        }
-                        catch
-                        {
-                            break;
-                        }
-
-                        if (bytesRead == 0)
-                        {
-                            break;
-                        }
-
-                        string data = Encoding.ASCII.GetString(buffer, 0, bytesRead);
-                        Console.WriteLine("Received from client: " + data);
-
-
-                        byte[] response = Encoding.ASCII.GetBytes(data);
-                        // Send the response to all connected clients 
-                        foreach (TcpClient connectedClient in clientBuffers.Keys)
-                        {
-                            NetworkStream connectedClientStream = connectedClient.GetStream();
-                            connectedClientStream.Write(response, 0, response.Length);
-                            connectedClientStream.Flush();
-                        }
-                    }
-
-                    client.Close();
-                    clientBuffers.Remove(client);
+                try
+                {
+                    bytesRead = clientStream.Read(buffer, 0, buffer.Length);
                 }
-            }
-        }
-    
+                catch
+                {
+                    break;
+                }
 
- 
+                if (bytesRead == 0)
+                {
+                    break;
+                }
+
+                string data = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+                Console.WriteLine("Received from client: " + data);
+
+
+                byte[] response = Encoding.ASCII.GetBytes(data);
+                // Send the response to all connected clients 
+                foreach (TcpClient connectedClient in clientBuffers.Keys)
+                {
+                    NetworkStream connectedClientStream = connectedClient.GetStream();
+                    connectedClientStream.Write(response, 0, response.Length);
+                    connectedClientStream.Flush();
+                }
+                SendData?.Invoke(this, new EventData(data));
+
+            }
+
+            client.Close();
+            clientBuffers.Remove(client);
+        }
+    }
+
+
+    public class EventData : EventArgs
+    {
+        public string data { get; }
+        public EventData(string data)
+        {
+
+           this. data = data;
+        }
+    }
+}
+
+
+
+
