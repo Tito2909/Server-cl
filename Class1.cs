90eb0c79df56b6397a8e88b1dc8acfd69b42a60a
tito2909 <omaar415@gmail.com>
2023-07-17 22:35:20 +0300

    80% done

--- Class1.cs
+++ Class1.cs
@@ -6,27 +6,31 @@ using System.Threading;
 using System.Collections.Generic;
 using System.Security.Cryptography.X509Certificates;
 using System.Windows;
-
+using System.Runtime.CompilerServices;
 
 namespace server
 {
     public class Server
     {
         private static TcpListener tcpListener;
-        private static readonly IPAddress ipAddress = IPAddress.Parse("192.168.113.92");
+        private static readonly IPAddress ipAddress = IPAddress.Parse("192.168.0.105");
         private static readonly int port = 2909;
         private static Dictionary<TcpClient, byte[]> clientBuffers = new Dictionary<TcpClient, byte[]>();
         private static TcpClient thirdSideClient;
         private static NetworkStream thirdSideStream;
         private static byte[] thirdSideBuffer = new byte[4096];
-        public event EventHandler<EventData> SendData;
-
+        public event EventHandler<string> SendData;
+        // public event EventHandler<EventData> DataToClients;
+        // Define the event for data received from the third side
+        public event EventHandler<string> ThirdSideDataReceived;
         public static void Main(string[] args)
         {
             Server server = new Server();
             server.SendData += SendToThirdSide;
+
+            server.ThirdSideDataReceived += SendDataToClients;
             server.Start();
-           
+
         }
         public void Start()
         {
@@ -43,8 +47,8 @@ namespace server
                 if (client != null)
                 {
                     clientBuffers.Add(client, new byte[4096]);
-                    Server server = new Server();
-                 
+
+
                     Thread clientThread = new Thread(new ParameterizedThreadStart(HandleClient));
                     clientThread.Start();
                 }
@@ -53,33 +57,28 @@ namespace server
                     Console.WriteLine("no client connected");
                 }
 
-                
 
-               
-               
+                //  string receivedData
+
+
             }
         }
-          // method to receive from third side and send the data to the clients
-        private static void ReceiveFromThirdSide()
+        // method to receive from third side and send the data to the clients
+        private void ReceiveFromThirdSide()
         {
             while (true)
             {
                 try
                 {
-                    byte[] buffer = new byte[4096];
-                    int bytesRead = thirdSideStream.Read(thirdSideBuffer, 0, buffer.Length);
-                    string receivedData = Encoding.ASCII.GetString(thirdSideBuffer, 0, bytesRead);
-                    Console.WriteLine("Received from third side: " + receivedData);
-
-                    // Send the received data to all connected clients
-                   
-                    foreach (TcpClient connectedClient in clientBuffers.Keys)
+                    if (thirdSideClient != null && thirdSideClient.Connected && thirdSideStream != null)
                     {
-                        NetworkStream connectedClientStream = connectedClient.GetStream();
-                        connectedClientStream.Write(thirdSideBuffer, 0, bytesRead);
-                        connectedClientStream.Flush();
+                        byte[] buffer = new byte[4096];
+                        int bytesRead = thirdSideStream.Read(thirdSideBuffer, 0, buffer.Length);
+                        string receivedDataFromThird = Encoding.ASCII.GetString(thirdSideBuffer, 0, bytesRead);
+                        Console.WriteLine("Received from third side: " + receivedDataFromThird);
+                        // Raise the event to send the received data to all connected clients
 
-
+                        ThirdSideDataReceived?.Invoke(this, receivedDataFromThird);
                     }
                 }
                 catch (Exception ex)
@@ -87,12 +86,29 @@ namespace server
                     Console.WriteLine("Error receiving data from the third side: " + ex.Message);
                     break;
                 }
+
+
+            }
+
+            // string ReceivedData = ReceiveFromThirdSide(); 
+            //return ReceivedData;    
+        }
+        // Send the received data to all connected clients
+        public static void SendDataToClients(Object source, string receivedDataFromThird)
+        {
+            foreach (TcpClient connectedClient in clientBuffers.Keys)
+            {
+                NetworkStream connectedClientStream = connectedClient.GetStream();
+                connectedClientStream.Write(thirdSideBuffer, 0, int.Parse(receivedDataFromThird));
+                connectedClientStream.Flush();
+
+
             }
         }
         // method to send the clients  data stored in the buffer to the third side 
-        static void SendToThirdSide(object source, EventData args)
+        static void SendToThirdSide(object source, string data)
         {
-            string thirdSideIPAddress = "ThirdSideIPAddress";
+            IPAddress thirdSideIPAddress = IPAddress.Parse("192.168.0.105");
             int thirdSidePort = 2808;
             thirdSideClient = new TcpClient();
 
@@ -104,7 +120,7 @@ namespace server
                 foreach (byte[] buffer in clientBuffers.Values)
                 {
                     // Send the buffer to the third side
-                    thirdSideStream.Write(buffer, 0, buffer.Length);
+                    thirdSideStream.Write(Encoding.ASCII.GetBytes(data), 0, data.Length);
                 }
 
                 Console.WriteLine("Buffers sent to the third side.");
@@ -126,6 +142,11 @@ namespace server
         {
             TcpClient client = (TcpClient)obj;
 
+            if (client == null)
+            {
+                Console.WriteLine("Invalid client object.");
+                return;
+            }
             NetworkStream clientStream = client.GetStream();
 
             byte[] buffer = clientBuffers[client]; // Get the client's buffer
@@ -151,7 +172,7 @@ namespace server
 
                 string data = Encoding.ASCII.GetString(buffer, 0, bytesRead);
                 Console.WriteLine("Received from client: " + data);
-
+                SendData?.Invoke(this, data);
 
                 byte[] response = Encoding.ASCII.GetBytes(data);
                 // Send the response to all connected clients 
@@ -161,27 +182,24 @@ namespace server
                     connectedClientStream.Write(response, 0, response.Length);
                     connectedClientStream.Flush();
                 }
-                SendData?.Invoke(this, new EventData(data));
+                // SendData?.Invoke(this, new EventData(data));
 
             }
 
             client.Close();
             clientBuffers.Remove(client);
         }
+
     }
 
 
-    public class EventData : EventArgs
-    {
-        public string data { get; }
-        public EventData(string data)
-        {
+    // public class EventData : EventArgs
+    //  {
+    //    public string ReceivedData { get; }
+    // public EventData(string receivedData)
+    // {
 
-           this. data = data;
-        }
-    }
+    //    ReceivedData = receivedData;
+    //  }
+
 }
-
-
-
-
